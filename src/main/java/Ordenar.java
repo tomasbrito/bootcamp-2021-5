fe1b369bf19c2fd247a1701b7cdf19e76e494118
fahelodev <fahelo.dev@gmail.com>
2021-08-17 11:57:13 -0400

    ejercicio 1

--- src/main/java/Ordenar.java
+++ src/main/java/Ordenar.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Ordenar {
+public class Ordenar {
+
+    /**
+     * Code Kata: Ordenar Cadena.
+     * Cada palabra de la cadena contendrá un solo número.
+     * Este número es la posición que debería tener la palabra en el resultado.
+     *
+     * Nota: Los números pueden ser del 1 al 9.
+     * Por lo tanto, 1 será la primera palabra (no 0).
+     *
+     * Si la cadena de entrada está vacía,
+     * devuelve una cadena vacía.
+     * Las palabras en la cadena de entrada solo contendrán números consecutivos válidos.
+     *
+     * Ejemplos:
+     *    "oraci4on un3a e2s Es1ta" -> "Es1ta e2s un3a oraci4on"
+     *
+     */
+
 }
