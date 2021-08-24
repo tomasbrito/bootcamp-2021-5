0e91ce59f86fa33b5eae91f24a67f471b7cb662e
Santiago Espinoza <Santiago.Espinoza@tsoftglobal.com>
2021-08-24 20:56:54 -0300

    Agregados archivos de KataCuatro y KataCinco

--- src/main/java/junit/sespinoza/KataCuatro.java
+++ src/main/java/junit/sespinoza/KataCuatro.java
@@ -6,50 +6,34 @@ public class KataCuatro {
     public static String sortCsvColumns(String csvFileContent) {
         String resultado = "";
         String filas[] = csvFileContent.split("\n");
-        String fila0[] =filas[0].split(";");
-        String filasSeparadas[][] = new String[filas.length][fila0.length];
+        String titleCopy[] =filas[0].split(";");
+        String filasSeparadas[][] = new String[filas.length][titleCopy.length];
         for (int i = 0; i < filas.length; i++) {
             filasSeparadas[i] = filas[i].split(";");
         }
-
-        boolean intercambio = true;
-        while (intercambio){
-            intercambio = false;
-            int i = 1;
-            while (i < fila0.length -1){
-                if(fila0[i - 1].compareTo(fila0[i])>0){
-                    String temp = filasSeparadas[0][i-1];
-                    filasSeparadas[0][i-1] = filasSeparadas[0][i];
-                    filasSeparadas[0][i] = temp;
-                    intercambio= true;
+        Arrays.sort(titleCopy,String.CASE_INSENSITIVE_ORDER);
+        for (int i = 0; i < titleCopy.length; i++) {
+            for (int j = 0; j < titleCopy.length; j++) {
+                if(titleCopy[i].equals(filasSeparadas[0][j])){
+                    for (int k = 0; k < filas.length; k++) {
+                        String aux = filasSeparadas[k][i];
+                        filasSeparadas[k][i] = filasSeparadas[k][j];
+                        filasSeparadas[k][j] = aux;
+                    }
                 }
-                i += 1;
             }
         }
-        for (int i = 0; i < fila0.length; i++) {
-            System.out.println(fila0[i]);
-            System.out.println(filasSeparadas[1][i]);
-            System.out.println(filasSeparadas[2][i]);
-        }
-
-
-        String arr1 = "";
-        String arr2 = "";
-        String arr3 = "";
-        for (int i = 0; i < fila0.length; i++) {
-            arr1 += fila0[i];
-            //arr2 += fila1[i];
-            //arr3 += fila2[i];
-            if (i != fila0.length -1){
-                arr1 += ";";
-                arr2 += ";";
-                arr3 += ";";
+        for (int i = 0; i < filas.length; i++) {
+            for (int j = 0; j < titleCopy.length; j++) {
+                resultado += filasSeparadas[i][j];
+                if(j != titleCopy.length-1) {
+                    resultado += ";";
+                }
+            }
+            if(i != filas.length - 1){
+                resultado += "\n";
             }
         }
-        arr1 += "\n";
-        arr2 += "\n";
-        resultado = arr1 + arr2 + arr3;
-
         return resultado;
     }
 }
