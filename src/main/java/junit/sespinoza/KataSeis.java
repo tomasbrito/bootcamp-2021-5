da45ef0856ffcfebd4ed8d00eb70d1ccf68fc886
Santiago Espinoza <Santiago.Espinoza@tsoftglobal.com>
2021-08-31 16:25:27 -0300

    Archivos agregados

---
+++ src/main/java/junit/sespinoza/KataSeis.java
@@ -0,0 +1,14 @@
+package junit.sespinoza;
+
+public class KataSeis {
+    public static String[] whoEatsWho(final String zoo) {
+        // Your code here
+        String[] wEats = {
+                ["no-eat","grass","little-fish","leaves","bug"],
+
+
+        return new String[]{zoo, zoo};
+    }
+
+}
+}
--- src/test/java/selenium/earaya/Selenium101Test.java
+++ src/test/java/selenium/earaya/Selenium101Test.java
@@ -1,4 +1,5 @@
-package selenium;
+/*
+package selenium.earaya;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
@@ -6,24 +7,36 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+<<<<<<< HEAD:src/test/java/selenium/Selenium101Test.java
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+=======
+import org.openqa.selenium.edge.EdgeDriver;
+>>>>>>> dbbfec2d806a78263e6a2831ad1b26436ede3e65:src/test/java/selenium/earaya/Selenium101Test.java
 
 public class Selenium101Test {
 
-    WebDriver driver;
+    WebDriver driver; // es la instancia a crear del navegador - firefox, chrome, safari
 
     @BeforeClass
     public static void SetUp(){
         System.out.println("Setup");
+<<<<<<< HEAD:src/test/java/selenium/Selenium101Test.java
         WebDriverManager.firefoxdriver().setup();
+=======
+        WebDriverManager.edgedriver().setup(); //vamos a crear una instancia de Google Chrome
+>>>>>>> dbbfec2d806a78263e6a2831ad1b26436ede3e65:src/test/java/selenium/earaya/Selenium101Test.java
     }
 
     @Before
     public void init(){
         System.out.println("init");
+<<<<<<< HEAD:src/test/java/selenium/Selenium101Test.java
         driver = new FirefoxDriver();
+=======
+        driver = new EdgeDriver();
+>>>>>>> dbbfec2d806a78263e6a2831ad1b26436ede3e65:src/test/java/selenium/earaya/Selenium101Test.java
     }
 
     @After
@@ -42,3 +55,4 @@ public class Selenium101Test {
 
 
 }
+*/
