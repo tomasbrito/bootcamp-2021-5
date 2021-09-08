7d6ac7eae62f290afa4ee7e71180a62a45e0b5c4
Gaston Becker <Gaston.Becker@tsoftglobal.com>
2021-09-07 21:59:58 -0300

    completado el tc-oo1

--- src/test/java/desafio/grupo3/TC_001.java
+++ src/test/java/desafio/grupo3/TC_001.java
@@ -1,4 +1,108 @@
 package desafio.grupo3;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class TC_001 {
+    WebDriver driver; // es la instancia a crear del navegador - firefox, chrome, safari
+
+    @BeforeClass
+    public static void SetUp(){
+        System.out.println("Setup");
+        WebDriverManager.chromedriver().setup();; //vamos a crear una instancia de Google Chrome
+    }
+
+    @Before
+    public void init(){
+        System.out.println("init");
+        driver = new ChromeDriver();
+        driver.manage().deleteAllCookies(); //borrar cookies
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void tc001() throws InterruptedException {
+        System.out.println("---Reserva alojamiento---");
+        driver.get("https://www.viajesfalabella.cl/hoteles/");
+
+        //Seleccionar el destino---------------------------------------------------------------------------------------------------------------------------
+        WebElement destino = driver.findElement(By.xpath("//*[@id=\"sboxContainer-hotels\"]/div/div/div[3]/div[2]/div[1]/div/div/div/div/div/input"));
+        destino.click();
+        Thread.sleep(1000);
+        destino.sendKeys("mia");
+        Thread.sleep(1000);
+        destino.sendKeys(Keys.ENTER);
+
+        //Seleccionar una fecha---------------------------------------------------------------------------------------------------------------------------
+        driver.findElement(By.xpath("//*[@id=\"sboxContainer-hotels\"]/div/div/div[3]/div[2]/div[2]/div/div/div[1]/div/input")).click();
+        WebElement fechaEntrada = driver.findElement(By.cssSelector("body > div.sbox-ui-datepicker-container.sbox-v4-components > div > div._dpmg2--months > div._dpmg2--month._dpmg2--o-3._dpmg2--month-active > div._dpmg2--dates > span:nth-child(18) > span._dpmg2--date-number"));
+        fechaEntrada.click();
+        Thread.sleep(1000);
+
+        WebElement fechaSalida = driver.findElement(By.cssSelector("body > div.sbox-ui-datepicker-container.sbox-v4-components > div > div._dpmg2--months > div:nth-child(2) > div._dpmg2--dates > span:nth-child(3) > span._dpmg2--date-number"));
+        fechaSalida.click();
+
+        driver.findElement(By.cssSelector("body > div.sbox-ui-datepicker-container.sbox-v4-components > div > div._dpmg2--date-footer > div._dpmg2--desktopFooter-btn-container > button._dpmg2--desktopFooter-button._dpmg2--desktopFooter-button-apply.sbox-3-btn.-lg.-primary > em")).click();
+
+        //Seleccionar habitaciones---------------------------------------------------------------------------------------------------------------------------
+        driver.findElement(By.xpath("//*[@id=\"sboxContainer-hotels\"]/div/div/div[3]/div[2]/div[3]/div/div/div[2]/div/div")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[2]/div[1]/div[2]/div[1]/div[2]/div/a[2]")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[2]/div[1]/div[2]/div[2]/div[2]/div/a[2]")).click();
+        Thread.sleep(1000);
+        Select edad = new Select(driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[2]/div/div/select")));
+        edad.selectByVisibleText("9 años");
+
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/a[1]")).click();
+
+        //Buscamos:---------------------------------------------------------------------------------------------------------------------------
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"sboxContainer-hotels\"]/div/div/div[3]/div[2]/div[4]/div/a/em")).click();
+        Thread.sleep(4000);
+
+        //Elegimos ver detalle del primer elemento----------------------------------------------------------------------------------------------------------------------------------
+        driver.findElement(By.xpath("/html/body/aloha-app-root/aloha-results/div/div/div/div[2]/div[2]/aloha-list-view-container/div[2]/div[1]/aloha-cluster-container/div/div/div[2]/aloha-cluster-pricebox-container/div/div[2]/div[2]/aloha-button/button/em")).click();
+        Thread.sleep(2000);
+        //aca cambia de ventana:
+        for (String winHandle : driver.getWindowHandles()) {
+            driver.switchTo().window(winHandle);
+            Thread.sleep(4000);
+        }
+
+        driver.findElement(By.xpath("/html/body/aloha-app-root/aloha-detail/div/div[2]/div[2]/div/aloha-infobox-container/aloha-infobox-wrapper-container/div/div/div/aloha-infobox-shopping-content/div/div[2]/aloha-button/button")).click();
+        //obtenemos precio por noche y descuento
+        //int precioXNoche = Integer.parseInt(driver.findElement(By.xpath("//*[@id=\"roompacks-container-wrapper\"]/aloha-roompacks-container/aloha-roompacks-grid-container/div[2]/div[1]/aloha-roompacks-group-container/div[2]/aloha-roompack-container/div[2]/aloha-roompack-price-container/aloha-summary-price/div/span[2]")).getText());
+        String precioXNoche = driver.findElement(By.xpath("//*[@id=\"roompacks-container-wrapper\"]/aloha-roompacks-container/aloha-roompacks-grid-container/div[2]/div[1]/aloha-roompacks-group-container/div[2]/aloha-roompack-container/div[2]/aloha-roompack-price-container/aloha-summary-price/div/span[2]")).getText();
+
+        driver.findElement(By.xpath("//*[@id=\"roompacks-container-wrapper\"]/aloha-roompacks-container/aloha-roompacks-grid-container/div[2]/div[2]/aloha-reservation-summary-container/div/aloha-next-step-button/aloha-button/button/em")).click();
+        Thread.sleep(4000);
+
+        driver.findElement(By.xpath("//*[@id=\"pricebox-overlay\"]/div[1]/div/button/em")).click();
+        Thread.sleep(5000);
+
+        //Comprobamos q los datos sean correctos
+        String cantPersonas = "Alojamiento para 4 personas";
+        double total = Double.parseDouble(precioXNoche) * 15;
+        double actual = Double.parseDouble(driver.findElement(By.xpath("//*[@id=\"chk-total-price\"]/div[2]/money/div/span[3]")).getText());
+
+        //Assert.assertEquals(cantPersonas, driver.findElement(By.xpath("//*[@id=\"pricebox-list-detail\"]/ul/div[1]/div[1]/p/span")));
+        Assert.assertEquals(total, actual, 30);
+
+    }
+
+
+
+    @After
+    public void close(){
+        System.out.println("After");
+        if(driver != null){
+            driver.close();
+        }
+    }
 }
--- src/test/java/desafio/grupo3/TC_003.java
+++ src/test/java/desafio/grupo3/TC_003.java
@@ -32,25 +32,9 @@ public class TC_003 {
 
     @Test
     public void TC_003() throws InterruptedException {
-        System.out.println("Error por falta de edad");
+        System.out.println("---Error por falta de edad---");
         driver.get("https://www.viajesfalabella.cl/hoteles/");
-        /*//Seleccionar una fecha
-        driver.findElement(By.xpath("//*[@id=\"sboxContainer-hotels\"]/div/div/div[3]/div[2]/div[2]/div/div/div[1]/div/input")).click();
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[5]/div[1]/div[4]/span[7]"));
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[5]/div[1]/div[4]/span[30]/span[1]"));
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[6]/div[2]/button[2]/em"));
-        */
-        //Seleccionar el destino
-        /*
-        WebElement destino = driver.findElement(By.xpath("//*[@id=\"sboxContainer-hotels\"]/div/div/div[3]/div[2]/div[1]/div/div/div/div"));
-        destino.click();
-        Thread.sleep(3000);
-        destino.sendKeys("Buenos");//El destino será en Buenos Aires
-        Thread.sleep(3000);
-        destino.sendKeys(Keys.ENTER);
-         */
-        //
+        //Seleccionar habitaciones
         driver.findElement(By.xpath("//*[@id=\"sboxContainer-hotels\"]/div/div/div[3]/div[2]/div[3]/div/div/div[2]/div/div")).click();
         driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[2]/div[1]/div[2]/div[2]/div[2]/div/a[2]")).click();
         driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/a[1]")).click();
