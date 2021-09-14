a5a1307696fb753bbbace1311fdcf110e9c0690b
fahelodev <fahelo.dev@gmail.com>
2021-09-14 12:23:21 -0300

    integracion con cucumber OK

--- src/test/java/bdd/earaya/pages/GoogleHomePage.java
+++ src/test/java/bdd/earaya/pages/GoogleHomePage.java
@@ -1,11 +1,10 @@
 package bdd.earaya.pages;
 
-import bdd.earaya.bases.SeleniumBase;
+import bdd.mentoria.bases.SeleniumBase;
 import static org.junit.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class GoogleHomePage extends SeleniumBase {
 
